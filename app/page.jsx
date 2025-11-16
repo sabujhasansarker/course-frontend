95fa1790d260d9fcc1b321f74c44381e496669e5
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2025-11-16 18:18:44 +0600

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,14 @@
 import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  const supabase = createClient(cookieStore);
+  const supabase = await createClient();
 
   const { data: todos } = await supabase.from("courses").select();
   console.log(todos);
 
   return (
     <ul>
-      {todos.map((data, i) => (
+      {todos?.map((data, i) => (
         <li key={data.id}>
           <h2>{data.instructor}</h2>
         </li>
